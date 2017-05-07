a418e952d9834dd402c5b35794f9fb4c41c3d16d
Karel Valek <kaja.valek@gmail.com>
2017-05-07 11:02:09 +0200

    Added & reworked find_pager and WIP auto_pager
    Signed-off-by: Karel Valek <kaja.valek@gmail.com>

--- pyhpBB_logic.py
+++ pyhpBB_logic.py
@@ -1,6 +1,22 @@
 import requests
 import re
 
+# Define general functions, that are not tied to any instance / class
+def find_pager(in_data, split_type = 'viewforum'): #
+    page_list = []
+    # Read the regex, split it by recognizable information
+    tmp_cache_list = re.search('onclick=\"jumpto.*', in_data.text).group(0).split(split_type +'.php')
+    for cache in tmp_cache_list:
+        tmp_data = re.search('start=(\d*)', cache)
+        if tmp_data and tmp_data.group(1) not in page_list:
+            page_list.append(tmp_data.group(1))
+    # first index: size and 'second' page, last index: last page reachable
+    return (int(page_list[0]), int(page_list[-1]))
+
+def auto_pager(in_page_size, in_last_page, url):
+    pass
+    
+
 class Forum(object):
     """
         docstring for Forum.
@@ -12,10 +28,26 @@ class Forum(object):
         self.session = requests.Session() # Cookies related data
         # Board related functions
         self.board_list = []
+        # Size related vars
+        self.page_size = None
+        self.last_page = None
 
-    # populate list of boards
+    def accept_rules(self): # Accept rules, if there is any front page. May be obsolete
+        self.session.post(self.url + 'enter.php', data = {'enter': 'enter'})
+
+    def load_page(self, url_sub_page = ''): # Load page and return result
+        return self.session.get(self.url + url_sub_page)
+
+    def login_forum(self, in_usr, in_passwd): # Login user
+        self.load_page('')
+        tmp_sid = self.session.cookies.items()[1][1] # Prepare SessionID from session
+        data_dict = {'username': in_usr, 'password': in_passwd,
+                     'sid': tmp_sid, 'login': 'login', 'redirect': 'index.php'}
+        self.session.post(self.url + 'ucp.php?mode=login', data = data_dict)
+
     def populate_boards(self): # Clean board URLS from HTML code using regexes
-        tmp_list = re.findall('viewforum.php\?.*', self.load_page('').text)
+        html_data = self.load_page('').text
+        tmp_list = re.findall('viewforum.php\?.*', html_data)
         # Finally, start cleaning
         for board_name in tmp_list:
             tmp_data = board_name.split("\"")
@@ -26,39 +58,12 @@ class Forum(object):
             tmp_board_object.topic_list = self.populate_topics(tmp_url)
             self.board_list.append(tmp_board_object)
 
-    def populate_topics(self, in_url):
-        tmp_list = re.findall('viewtopic.php.*', self.load_page(in_url).text)
-        final_list = []
-        for topic in tmp_list:
-            # Fix to search only string without start=0 and title only-check
-            if "class=\"topictitle\"" in topic:
-                tmp_url = re.search('viewtopic.php.*?"', topic).group(0).replace('amp', '').replace(';', '')[:-1]
-                tmp_name = re.search('topictitle">(.*)</a>', topic).group(1)
-                final_list.append(ForumTopic(tmp_url, tmp_name))
-            else:
-                pass
-        return final_list
-
-    def accept_rules(self): # Accept rules, if there is any front page
-        self.session.post(self.url + 'enter.php', data = {'enter': 'enter'})
-
-    def load_page(self, url_sub_page = ''): # Load page and return result
-        return self.session.get(self.url + url_sub_page)
-
-    def fix_url(self, forced_suffix = 'index.php'): # Fix forum url to match real URL, as it breaks login
-        self.url = self.session.get(self.url + forced_suffix).url
-
-    def login_forum(self, in_usr, in_passwd): # Login user
-        tmp_sid = self.session.cookies.items()[1][1] # Prepare SessionID from session
-        data_dict = {'username': in_usr, 'password': in_passwd,
-                     'sid': tmp_sid, 'login': 'login', 'redirect': 'index.php'}
-        self.session.post(self.url + 'ucp.php?mode=login', data = data_dict)
-
 class ForumBoard(object):
     """
         docstring for ForumBoard.
     """
-    def __init__(self, url, name):
+    def __init__(self, forum_url, url, name):
+        self.forum_url = forum_url
         self.url = url
         self.name = name
         self.topic_list = []
@@ -70,4 +75,6 @@ class ForumTopic(object):
     def __init__(self, url, name):
         self.url = url
         self.name = name
-        self.last_post = None
+        self.page_size = None
+        self.last_page = None
+        self.content = None
